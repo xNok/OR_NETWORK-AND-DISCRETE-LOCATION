24c715e2a0d741c20833ed1d6fa3018de1845a9c
Alexandre Couedelo <nokwebspace@gmail.com>
2019-01-03 17:31:38 -0500

    refact(uflp): new var definition

--- PythonLib/solvers/uflp.py
+++ PythonLib/solvers/uflp.py
@@ -3,22 +3,19 @@ from docplex.mp.model import Model
 def uflp(I, J, f, c, name='UFLP'):
     ###################
     # create one model instance
-    m = Model(name='UFLP')
+    m = Model(name=name)
 
     ###################
     # Define variables
 
     # x(i,j) equals 1 if arc ij is in the solution
-    X = {
-        (i,j): m.binary_var(name='X_%d_%d' % (i,j))
-         for i in I for j in J
-    } 
+    X = m.binary_var_dict([(i,j) 
+                          for i in I
+                          for j in J], name="X")
     # y(j) equales 1 if node j is in the solution
-    Y = {
-        (j): m.binary_var(name='Y_%d' % (j))
-        for j in J
-    }
-
+    Y = m.binary_var_dict([(j) 
+                          for j in J], name="Y")
+    
     ###################
     # Define Objective
     m.minimize(m.sum(X[i,j] * c[i][j] for i in I for j in J) \
@@ -35,5 +32,7 @@ def uflp(I, J, f, c, name='UFLP'):
     for i in I:
         for j in J:
             m.add_constraint(X[i,j] <= Y[j], ctname='flow_%s_%s' % (i,j))
+    
+    m.solve()
             
-    return m
+    return m, X, Y
