2c70cc4887c5abfc339e218be310bac73e854b16
Alexandre Couedelo <nokwebspace@gmail.com>
2019-02-12 22:42:17 -0500

    install pythonLib as package

--- PythonLib/setup.py
+++ PythonLib/setup.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 
-setup(name='NetAndLocUtil',
+setup(name='netandloc',
       version='1.0',
       description='Python Library To facilitate the manipulation of datasets from OR_NETWORK_AND_DISCRETE_LOCATION',
       author='xNok',
       author_email='nokwebspace@gmail.com',
       url='https://github.com/xNok/OR_NETWORK-AND-DISCRETE-LOCATION',
-      packages=['dataset', 'dataviz'],
+      packages=find_packages(),
      )
